ca1caf367c758ec640567cfffcdb91d7451d5d34
Ma-XD <7721772@mail.ru>
2024-04-30 18:19:18 +0300

    Test calc

--- TM-lab-4/src/main/kotlin/TestParser.kt
+++ TM-lab-4/src/main/kotlin/TestParser.kt
@@ -4,9 +4,9 @@ import java.io.File
 const val PATH = "src/main/resources"
 
 fun main() {
-    val outputFile = File(File(PATH), "tree")
+    val outputFile = File(File(PATH), "calc_tree")
 
     val tree = Parser().parse("16 / (3 + 1) / 2")
-//    TreeVisualizer(tree).visualize(outputFile)
+    TreeVisualizer(tree).visualize(outputFile)
     println(tree.res ?: "tree doesn't return anything")
 }
